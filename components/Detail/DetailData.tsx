3e92f1f2d312347f36078dae4266ce3b7e5b2abd
nopupu <apple912162@gmail.com>
2024-01-18 02:28:27 +0800

    add use client to MockData

--- components/Detail/DetailData.tsx
+++ components/Detail/DetailData.tsx
@@ -1,31 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const DetailData = () => {
     const [earnings, setEarnings] = useState<number | null>(null);
 
-    useEffect(() => {
-        // Replace 'YOUR_API_ENDPOINT' with the actual API endpoint for retrieving wallet data
-        fetch('API_ENDPOINT', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+
+    // Replace 'API_ENDPOINT' with the actual API endpoint for retrieving wallet data
+    fetch('API_ENDPOINT', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
         })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                // Assuming the API response contains the earnings data
-                setEarnings(data.earnings);
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-                // Handle the error, you might want to set an error state for user feedback
-            });
-    }, []);
+        .then(data => {
+            // Assuming the API response contains the earnings data
+            setEarnings(data.earnings);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            // Handle the error, you might want to set an error state for user feedback
+        });
 
     return <>{earnings !== null && <p>You earned $ {earnings} in total!</p>}</>;
 };
